9ef903dbc65bdf824f3ec95c6697cdf7e9c0a7a0
Zinenko <zinkalexvvv@gmail.com>
2024-03-27 11:34:58 +0300

    Commit

--- src/main/java/mvc/MyController.java
+++ src/main/java/mvc/MyController.java
@@ -1,2 +1,19 @@
-package mvc;public class MyController {
-}
+package mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+    @Controller
+    public class MyController {
+
+        @RequestMapping("/first-view")
+        public String showFirstView() {
+            return "first-view";
+        }
+
+    }
+
+
+
+
